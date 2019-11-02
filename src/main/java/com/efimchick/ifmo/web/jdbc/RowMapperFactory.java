463bd2c0248f8df6579a90fff1fcde83ca71986c
belonel <anatot92>
2019-11-02 18:11:05 +0300

    Second task completed.

--- src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
@@ -1,10 +1,42 @@
 package com.efimchick.ifmo.web.jdbc;
 
 import com.efimchick.ifmo.web.jdbc.domain.Employee;
+import com.efimchick.ifmo.web.jdbc.domain.FullName;
+import com.efimchick.ifmo.web.jdbc.domain.Position;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class RowMapperFactory {
 
     public RowMapper<Employee> employeeRowMapper() {
-        throw new UnsupportedOperationException();
+        RowMapper<Employee> rw = new RowMapper() {
+            @Override
+            public Employee mapRow(ResultSet rs) {
+                try {
+                    BigInteger id = BigInteger.valueOf(rs.getInt("id"));
+
+                    String firstName = rs.getString("firstname");
+                    String lastName = rs.getString("lastname");
+                    String middleName = rs.getString("middlename");
+                    FullName fullName = new FullName(firstName, lastName, middleName);
+
+                    Position position = Position.valueOf(rs.getString("position"));
+
+                    LocalDate hired = rs.getDate("hiredate").toLocalDate();
+
+                    BigDecimal salary = rs.getBigDecimal("salary");
+
+                    return new Employee(id, fullName, position, hired, salary);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        };
+        return rw;
     }
 }
